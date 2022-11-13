3eba13167d82c176f8fb55485fda2f5556ffd5d8
guptaharsh940 <guptaharsh940@gmail.com>
2022-11-13 22:01:14 +0530

    Fucked up

--- lib/information.dart
+++ lib/information.dart
@@ -67,10 +67,61 @@ class _InfoPageState extends State<InfoPage> {
 
   @override
   Widget build(BuildContext context) {
-    
     // getAttendance();
-    return InfoWid(
-        );
+      if (kDebugMode) {
+        print("Started Fuck");
+      }
+    return DefaultTabController(
+        length: 2,
+        child: Scaffold(
+            appBar: AppBar(
+              bottom: const TabBar(
+                tabs: [
+                  Tab(text: 'Summary'),
+                  Tab(text: 'Empty Rooms'),
+                ],
+              ),
+              title: const Text('Result'),
+            ),
+            body: TabBarView(children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                // ignore: prefer_const_literals_to_create_immutables
+                children: [
+                  Text(
+                    'Current Count : 0',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text('Max Count : 450', style: TextStyle(fontSize: 20)),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text('Empty Rooms : 120', style: TextStyle(fontSize: 20)),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text('Last Updated : 12-11-22/2:38PM',
+                      style: TextStyle(fontSize: 20)),
+                ],
+              ),
+              Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Card(
+                        child: ListView.builder(
+                            itemCount: rooms.length,
+                            itemBuilder: (context, index) {
+                              return InfoWid(
+                                  roomNo: rooms[index].roomNo,
+                                  totalOccupants: rooms[index].totalOccupants,
+                                  currentOccupants:
+                                      rooms[index].currentOccupants);
+                            }))
+                  ])
+            ])));
   }
 }
 
@@ -90,250 +141,34 @@ class InfoWid extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          bottom: const TabBar(
-            tabs: [
-              Tab(text: 'Summary'),
-              Tab(text: 'Empty Rooms'),
-            ],
-          ),
-          title: const Text('Result'),
-        ),
-        body: TabBarView(
+    return Card(
+      child: ListTile(
+        title: Row(
           children: [
+            Column(children: [
+              Text('Room No. -  '),
+              SizedBox(
+                height: 5,
+              ),
+              Text('Occupants - '),
+              SizedBox(
+                height: 5,
+              ),
+              Text('         12-11-22 / 12:38'),
+            ]),
             Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              // ignore: prefer_const_literals_to_create_immutables
               children: [
-                Text(
-                  'Current Count : 234',
-                  style: TextStyle(fontSize: 20),
-                ),
                 SizedBox(
-                  height: 20,
+                  height: 4,
                 ),
-                Text('Max Count : 450', style: TextStyle(fontSize: 20)),
+                Text('303'),
                 SizedBox(
-                  height: 20,
+                  height: 6,
                 ),
-                Text('Empty Rooms : 120', style: TextStyle(fontSize: 20)),
+                Text('3'),
                 SizedBox(
-                  height: 20,
-                ),
-                Text('Last Updated : 12-11-22/2:38PM',
-                    style: TextStyle(fontSize: 20)),
-              ],
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Card(
-                  child: ListTile(
-                    title: Row(
-                      children: [
-                        Column(children: [
-                          Text('Room No. -  '),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Text('Occupants - '),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Text('         12-11-22 / 12:38'),
-                        ]),
-                        Column(
-                          children: [
-                            SizedBox(
-                              height: 4,
-                            ),
-                            Text('303'),
-                            SizedBox(
-                              height: 6,
-                            ),
-                            Text('3'),
-                            SizedBox(
-                              height: 27,
-                            )
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    title: Row(
-                      children: [
-                        Column(children: [
-                          Text('Room No. -  '),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Text('Occupants - '),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Text('         12-11-22 / 12:38'),
-                        ]),
-                        Column(
-                          children: [
-                            SizedBox(
-                              height: 4,
-                            ),
-                            Text('303'),
-                            SizedBox(
-                              height: 6,
-                            ),
-                            Text('3'),
-                            SizedBox(
-                              height: 27,
-                            )
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    title: Row(
-                      children: [
-                        Column(children: [
-                          Text('Room No. -  '),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Text('Occupants - '),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Text('         12-11-22 / 12:38'),
-                        ]),
-                        Column(
-                          children: [
-                            SizedBox(
-                              height: 4,
-                            ),
-                            Text('303'),
-                            SizedBox(
-                              height: 6,
-                            ),
-                            Text('3'),
-                            SizedBox(
-                              height: 27,
-                            )
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    title: Row(
-                      children: [
-                        Column(children: [
-                          Text('Room No. -  '),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Text('Occupants - '),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Text('         12-11-22 / 12:38'),
-                        ]),
-                        Column(
-                          children: [
-                            SizedBox(
-                              height: 4,
-                            ),
-                            Text('303'),
-                            SizedBox(
-                              height: 6,
-                            ),
-                            Text('3'),
-                            SizedBox(
-                              height: 27,
-                            )
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    title: Row(
-                      children: [
-                        Column(children: [
-                          Text('Room No. -  '),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Text('Occupants - '),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Text('         12-11-22 / 12:38'),
-                        ]),
-                        Column(
-                          children: [
-                            SizedBox(
-                              height: 4,
-                            ),
-                            Text('303'),
-                            SizedBox(
-                              height: 6,
-                            ),
-                            Text('3'),
-                            SizedBox(
-                              height: 27,
-                            )
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    title: Row(
-                      children: [
-                        Column(children: [
-                          Text('Room No. -  '),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Text('Occupants - '),
-                          SizedBox(
-                            height: 5,
-                          ),
-                          Text('         12-11-22 / 12:38'),
-                        ]),
-                        Column(
-                          children: [
-                            SizedBox(
-                              height: 4,
-                            ),
-                            Text('303'),
-                            SizedBox(
-                              height: 6,
-                            ),
-                            Text('3'),
-                            SizedBox(
-                              height: 27,
-                            )
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
+                  height: 27,
+                )
               ],
             ),
           ],
